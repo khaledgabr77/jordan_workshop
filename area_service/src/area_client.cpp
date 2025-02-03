2df9606b57b0011668c2cef2ef1c15f4fbdce092
khaledgabr77 <khaledgabr77@gmail.com>
2025-02-03 17:38:50 +0300

    add service quiz

---
+++ area_service/src/area_client.cpp
@@ -0,0 +1,10 @@
+#include "rclcpp/rclcpp.hpp"
+#include "area_interfaces/srv/calculate_area.hpp"
+
+// TODO: Implement service client class
+
+
+int main(int argc, char **argv) {
+    // TODO: Initialize ROS 2 node and send a request to the server
+
+}
---
+++ area_service/src/area_server.cpp
@@ -0,0 +1,10 @@
+#include "rclcpp/rclcpp.hpp"
+#include "area_interfaces/srv/calculate_area.hpp"
+
+// TODO: Implement service server class
+
+
+int main(int argc, char **argv) {
+    // TODO: Initialize ROS 2 node and spin the service server
+
+}
